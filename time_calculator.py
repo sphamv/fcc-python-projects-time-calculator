0c6f3ec1f22a45d880a868d03f73725665ce9a9f
sung pham <s.phamv@hotmail.com>
2020-11-08 16:15:28 +0100

    Weekdays added

--- time_calculator.py
+++ time_calculator.py
@@ -1,10 +1,15 @@
-def add_time(start, duration):
+def add_time(*args):
     HOURS_DAY = 24
     HOURS_WEEK = 168
     MINUTES_DAY = 1440
     MINUTES_WEEK = 10080
+    WEEKDAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
     
-
+    start = args[0]
+    duration = args[1]
+    weekday = args[2].capitalize()
+    weekday_pos = WEEKDAYS.index(weekday)
+    
     if len(start.split()) !=2 or int(start.split()[0].split(":")[0])>12 or start.split()[1] not in ["AM", "PM"]: 
         return "Error: Invalid starting time format"
     
@@ -43,9 +48,9 @@ def add_time(start, duration):
                 new_minutes = 0
             
             if new_minutes < 10:
-                new_time = str(new_hours) + ":" + "0" + str(new_minutes) + " " + daytime
+                new_time = str(new_hours) + ":" + "0" + str(new_minutes) + " " + daytime + ", " + weekday
             else:
-                new_time = str(new_hours) + ":" + str(new_minutes) + " " + daytime
+                new_time = str(new_hours) + ":" + str(new_minutes) + " " + daytime + ", " + weekday
         else:
             daytime = "PM"
             #print(daytime)
@@ -62,13 +67,20 @@ def add_time(start, duration):
             
             # keep the double digit format if minutes are less than 10
             if new_minutes < 10:
-                new_time = str(new_hours) + ":" + "0" + str(new_minutes) + " " + daytime
+                new_time = str(new_hours) + ":" + "0" + str(new_minutes) + " " + daytime + ", " + weekday
             else:
-                new_time = str(new_hours) + ":" + str(new_minutes) + " " + daytime
+                new_time = str(new_hours) + ":" + str(new_minutes) + " " + daytime + ", " + weekday
         return new_time
     
     # Display the time if the new time is on the next day or more
     else: 
+        new_weekday_pos = weekday_pos + days
+        if new_weekday_pos < 7:
+            new_weekday = WEEKDAYS[new_weekday_pos]
+        else:
+            new_weekday_pos = int((days%7))
+            new_weekday = WEEKDAYS[new_weekday_pos]
+
         rem_minutes = sum_minutes % MINUTES_DAY
         # print("rem_minutes: ", str(rem_minutes))
         if rem_minutes <=(MINUTES_DAY/2):
@@ -82,9 +94,9 @@ def add_time(start, duration):
                 new_minutes = 0
                 
             if new_minutes < 10:
-                new_time = str(new_hours) + ":" + "0" + str(new_minutes) + " " + daytime
+                new_time = str(new_hours) + ":" + "0" + str(new_minutes) + " " + daytime + ", " + new_weekday
             else:
-                new_time = str(new_hours) + ":" + str(new_minutes) + " " + daytime
+                new_time = str(new_hours) + ":" + str(new_minutes) + " " + daytime + ", " + new_weekday
 
             # print ("new time: ", new_time)
 
@@ -103,9 +115,9 @@ def add_time(start, duration):
             
             # keep the double digit format if minutes are less than 10
             if new_minutes< 10:
-                new_time = str(new_hours) + ":" + "0" + str(new_minutes) + " " + daytime
+                new_time = str(new_hours) + ":" + "0" + str(new_minutes) + " " + daytime + ", " + new_weekday
             else:
-                new_time = str(new_hours) + ":" + str(new_minutes) + " " + daytime
+                new_time = str(new_hours) + ":" + str(new_minutes) + " " + daytime + ", " + new_weekday
         
         if days < 2:
             return new_time + " (next day)"
@@ -115,7 +127,9 @@ def add_time(start, duration):
 
     
 
-print(add_time("3:00 PM", "3:10"))
-print(add_time("11:43 AM", "00:20"))
-print(add_time("10:10 PM", "3:30"))
-print(add_time("6:30 PM", "205:12"))
+# print(add_time("3:00 PM", "3:10"))
+# print(add_time("11:43 AM", "00:20"))
+# print(add_time("10:10 PM", "3:30"))
+# print(add_time("6:30 PM", "205:12"))
+#print(add_time("11:30 AM", "192:32", "mOnDay"))
+#print (add_time("11:43 PM", "24:20", "tueSday"))
